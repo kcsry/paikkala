9145a415ad9c2ccb197c5b5d2e7742a7f963e4b3
Aarni Koskela <akx@iki.fi>
2018-02-25 16:45:02 +0200

    Embetter admin

--- paikkala/admin.py
+++ paikkala/admin.py
@@ -9,8 +9,24 @@ class RowInline(admin.TabularInline):
 
 class ZoneAdmin(admin.ModelAdmin):
     inlines = [RowInline]
+    list_display = ('name', 'capacity')
 
 
-admin.site.register(Program)
-admin.site.register(Ticket)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ('name', 'reservation_start', 'reservation_end', 'reserved_tickets', 'max_tickets')
+
+    def reserved_tickets(self, instance):
+        return instance.tickets.count()
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_select_related = ('program', 'zone', 'row')
+    list_filter = ('program', 'zone')
+    search_fields = ('user__username',)
+    list_display = ('id', 'program', 'zone', 'row', 'number')
+    raw_id_fields = ('user',)
+
+
+admin.site.register(Program, ProgramAdmin)
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Zone, ZoneAdmin)
