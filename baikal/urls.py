db9b54477cbf766bcf80e4f9ae3621fbffb619cd
Aarni Koskela <akx@iki.fi>
2018-05-12 19:18:59 +0300

    Add tests for views

--- baikal/urls.py
+++ baikal/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.conf.urls import url
 
 from baikal.views import IndexView
-from paikkala.views import InspectionView, ReservationView
+from paikkala.views import InspectionView, ReservationView, RelinquishView
 
 urlpatterns = [
     url('^admin/', admin.site.urls),
+    url('^relinquish/(?P<pk>\d+)/$', RelinquishView.as_view(), name='relinquish'),
     url('^reserve/(?P<pk>\d+)/$', ReservationView.as_view(template_name='reserve.html'), name='reserve'),
     url('^inspect/(?P<pk>\d+)/(?P<key>.+?)/$', InspectionView.as_view(template_name='inspect.html'), name='inspect'),
     url('^$', IndexView.as_view()),
---
+++ paikkala/tests/test_views.py
@@ -0,0 +1,46 @@
+import pytest
+from django.test import Client
+from django.urls import reverse
+from django.utils.crypto import get_random_string
+from django.utils.encoding import force_text
+
+from paikkala.models import Ticket
+
+
+@pytest.fixture
+def user_client(django_user_model):
+    user = django_user_model.objects.create_user(username=get_random_string())
+    client = Client()
+    client.force_login(user)
+    client.user = user
+    return client
+
+
+@pytest.mark.django_db
+def test_reserve(jussi_program, user_client):
+    assert jussi_program.is_reservable()
+    reserve_url = reverse('reserve', kwargs={'pk': jussi_program.pk})
+    assert user_client.get(reserve_url).status_code == 200
+    assert user_client.post(reserve_url, {
+        'zone': jussi_program.zones[0].pk,
+        'count': 5,
+    }).status_code == 302
+    assert Ticket.objects.filter(program=jussi_program, user=user_client.user).count() == 5
+
+
+@pytest.mark.django_db
+def test_relinquish(jussi_program, user_client):
+    assert jussi_program.is_reservable()
+    ticket, = jussi_program.reserve(zone=jussi_program.zones[0], count=1, user=user_client.user)
+    user_client.post(reverse('relinquish', kwargs={'pk': ticket.pk}), {'key': ticket.key})
+    assert not Ticket.objects.filter(program=jussi_program, user=user_client.user).exists()
+
+
+@pytest.mark.django_db
+def test_inspect(jussi_program, user_client):
+    assert jussi_program.is_reservable()
+    tickets = list(jussi_program.reserve(zone=jussi_program.zones[0], count=6, user=user_client.user))
+    ticket = tickets[0]
+    resp = user_client.get(reverse('inspect', kwargs={'pk': ticket.pk, 'key': ticket.key}))
+    assert len(resp.context['tickets']) == 6
+    assert jussi_program.name in force_text(resp.render().content)
