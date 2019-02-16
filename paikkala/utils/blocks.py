8ce416ab12ed61cb5f4fe3f681ca36dffd768e46
Aarni Koskela <akx@iki.fi>
2019-02-16 12:11:17 +0200

    Un-free get_per_program_blocks

--- paikkala/utils/blocks.py
+++
@@ -1,8 +0,0 @@
-from collections import defaultdict
-
-
-def get_per_program_blocks(program, zone):
-    blocks_by_row_id = defaultdict(set)
-    for block in program.blocks.filter(row__zone=zone):
-        blocks_by_row_id[block.row_id] |= block.get_excluded_set()
-    return blocks_by_row_id
