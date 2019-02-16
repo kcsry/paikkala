1c420847e7c166f46b23d838a04a775695523f3a
Aarni Koskela <akx@iki.fi>
2019-02-16 11:36:17 +0200

    Fix invalid escape warnings

--- baikal/urls.py
+++ baikal/urls.py
@@ -5,9 +5,9 @@ from baikal.views import IndexView, InspectionView
 from paikkala.views import ReservationView, RelinquishView
 
 urlpatterns = [
-    url('^admin/', admin.site.urls),
-    url('^relinquish/(?P<pk>\d+)/$', RelinquishView.as_view(), name='relinquish'),
-    url('^reserve/(?P<pk>\d+)/$', ReservationView.as_view(template_name='reserve.html'), name='reserve'),
-    url('^inspect/(?P<pk>\d+)/(?P<key>.+?)/$', InspectionView.as_view(template_name='inspect.html'), name='inspect'),
-    url('^$', IndexView.as_view()),
+    url(r'^admin/', admin.site.urls),
+    url(r'^relinquish/(?P<pk>\d+)/$', RelinquishView.as_view(), name='relinquish'),
+    url(r'^reserve/(?P<pk>\d+)/$', ReservationView.as_view(template_name='reserve.html'), name='reserve'),
+    url(r'^inspect/(?P<pk>\d+)/(?P<key>.+?)/$', InspectionView.as_view(template_name='inspect.html'), name='inspect'),
+    url(r'^$', IndexView.as_view()),
 ]
