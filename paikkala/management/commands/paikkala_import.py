06f766b36ba9a5423f49012c0342d61fb83c41e7
Aarni Koskela <akx@iki.fi>
2019-04-28 13:37:27 +0300

    Make paikkala_import more verbose and more idempotent

--- paikkala/management/commands/paikkala_import.py
+++ paikkala/management/commands/paikkala_import.py
@@ -6,18 +6,25 @@ from paikkala.utils.importer import read_csv_file, import_zones
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('zone_filename')
-        parser.add_argument('qualifier_filename', required=False)
+        parser.add_argument('--zone-filename', '-z', required=True)
+        parser.add_argument('--qualifier-filename', '-q', required=False)
+        parser.add_argument('--default-room-name', default='Room')
 
     @atomic
-    def handle(self, zone_filename, qualifier_filename, **options):
+    def handle(self, zone_filename, qualifier_filename, default_room_name, **options):
         z_rows = list(read_csv_file(zone_filename))
         q_rows = (list(read_csv_file(qualifier_filename)) if qualifier_filename else ())
-        for zone in import_zones(row_csv_list=z_rows, qualifier_csv_list=q_rows):
+        for zone in import_zones(
+            row_csv_list=z_rows,
+            qualifier_csv_list=q_rows,
+            default_room_name=default_room_name,
+            verbose=True,
+        ):
             self.stdout.write(
-                '%s: capacity %d, rows %s' % (
+                '%s: capacity %d, rows %s, %d qualifiers' % (
                     zone,
                     zone.capacity,
                     ', '.join(name for name in zone.rows.values_list('name', flat=True)),
+                    zone.seat_qualifiers.count(),
                 )
             )
--- paikkala/utils/importer.py
+++ paikkala/utils/importer.py
@@ -16,34 +16,48 @@ def read_csv_file(filename, separator=','):
         yield from read_csv(infp, separator)
 
 
-def import_zones(row_csv_list, qualifier_csv_list=(), default_room_name='Room'):
+def import_zones(row_csv_list, qualifier_csv_list=(), default_room_name='Room', verbose=False):
     rooms_zones = {}
 
-    def get_or_create_room(data):
+    def get_or_create_zone(data):
         room_name = data.get('room', default_room_name)
         zone_name = data['zone']
         rz_key = (room_name, zone_name)
         zone = rooms_zones.get(rz_key)
         if not zone:
-            room, _ = Room.objects.get_or_create(name=room_name)
-            zone = rooms_zones[rz_key] = Zone.objects.create(room=room, name=zone_name)
+            room, room_created = Room.objects.get_or_create(name=room_name)
+            zone, zone_created = Zone.objects.get_or_create(room=room, name=zone_name)
+            if verbose:
+                if room_created:
+                    print('Room %s (id %s) created' % (room, room.id))
+                if zone_created:
+                    print('Zone %s (id %s) created' % (zone, zone.id))
+            rooms_zones[rz_key] = zone
         return zone
 
     for r_dict in row_csv_list:
-        zone = get_or_create_room(r_dict)
-        row = zone.rows.create(
-            start_number=int(r_dict['start']),
-            end_number=int(r_dict['end']),
+        zone = get_or_create_zone(r_dict)
+        row, row_created = zone.rows.get_or_create(
             name=int(r_dict['row']),
+            defaults=dict(
+                start_number=int(r_dict['start']),
+                end_number=int(r_dict['end']),
+            ),
         )
+        if verbose and row_created:
+            print('Row %s (id %s) created' % (row, row.id))
+
+
         assert row.capacity > 0
 
     for q_dict in qualifier_csv_list:
-        zone = get_or_create_room(q_dict)
-        zone.seat_qualifiers.create(
+        zone = get_or_create_zone(q_dict)
+        qual, qual_created = zone.seat_qualifiers.get_or_create(
             start_number=int(q_dict['start']),
             end_number=int(q_dict['end']),
             text=q_dict['text'],
         )
+        if verbose and qual_created:
+            print('Qualifier %s (id %s) created' % (qual, qual.id))
 
     return list(rooms_zones.values())
