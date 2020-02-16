0323d052f6022c64f68e601c255c0610e6ea8bf8
Aarni Koskela <akx@iki.fi>
2020-02-16 17:46:05 +0200

    Override InspectionView prop in urls

--- baikal/urls.py
+++ baikal/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from baikal.views import IndexView, InspectionView
-from paikkala.views import RelinquishView, ReservationView
+from baikal.views import IndexView
+from paikkala.views import InspectionView, RelinquishView, ReservationView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^relinquish/(?P<pk>\d+)/$', RelinquishView.as_view(), name='relinquish'),
     url(r'^reserve/(?P<pk>\d+)/$', ReservationView.as_view(template_name='reserve.html'), name='reserve'),
-    url(r'^inspect/(?P<pk>\d+)/(?P<key>.+?)/$', InspectionView.as_view(template_name='inspect.html'), name='inspect'),
+    url(r'^inspect/(?P<pk>\d+)/(?P<key>.+?)/$', InspectionView.as_view(require_same_user=False, template_name='inspect.html'), name='inspect'),
     url(r'^$', IndexView.as_view()),
 ]
