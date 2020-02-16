51cc11f28ab244475d5e30e9e6c60d775f21da4d
Aarni Koskela <akx@iki.fi>
2020-02-16 18:18:01 +0200

    Rename baikal reserve to generic form in print preparation

--- baikal/urls.py
+++ baikal/urls.py
@@ -7,7 +7,7 @@ from paikkala.views import InspectionView, RelinquishView, ReservationView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^relinquish/(?P<pk>\d+)/$', RelinquishView.as_view(require_same_user=False), name='relinquish'),
-    url(r'^reserve/(?P<pk>\d+)/$', ReservationView.as_view(template_name='reserve.html'), name='reserve'),
+    url(r'^reserve/(?P<pk>\d+)/$', ReservationView.as_view(template_name='generic_form.html'), name='reserve'),
     url(r'^inspect/(?P<pk>\d+)/(?P<key>.+?)/$', InspectionView.as_view(require_same_user=False, template_name='inspect.html'), name='inspect'),
     url(r'^$', IndexView.as_view()),
 ]
