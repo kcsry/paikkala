bda586cf8bb8dccd09b562c31f6cc327aa1ebea4
Aarni Koskela <akx@iki.fi>
2022-06-19 22:47:44 +0300

    Add admin for seat qualifiers

--- paikkala/admin/zone.py
+++ paikkala/admin/zone.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
-from paikkala.models import Row
+from django.db.models import QuerySet
+from django.http import HttpRequest
+
+from paikkala.models import Row, SeatQualifier
 
 
 class RowInline(admin.TabularInline):
     model = Row
     readonly_fields = ('capacity',)
 
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).select_related('zone', 'zone__room')
+
+
+class SeatQualifierInline(admin.TabularInline):
+    model = SeatQualifier
+
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
+        return super().get_queryset(request).select_related('zone')
+
 
 class ZoneAdmin(admin.ModelAdmin):
-    inlines = [RowInline]
+    inlines = [RowInline, SeatQualifierInline]
     list_select_related = ('room',)
     list_display = ('name', 'room', 'capacity')
     list_filter = ('room',)
