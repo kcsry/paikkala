937acd6b93d7b504a32dd4da03eebb40b1ded735
Anssi Matti Helin <amhelin@iki.fi>
2025-05-10 15:43:50 +0300

    change: bulk insert tickets in `Row.reserve`

--- paikkala/models/rows.py
+++ paikkala/models/rows.py
@@ -4,16 +4,18 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.core.exceptions import ValidationError
 from django.db import models
 
+from paikkala.models.tickets import Ticket, generate_key
 from paikkala.utils.ranges import parse_number_set, validate_number_set
 from paikkala.utils.runs import find_runs, following_integer
 
 if TYPE_CHECKING:
     from paikkala.models.programs import Program
-    from paikkala.models.tickets import Ticket
 
 
 class Row(models.Model):
-    zone = models.ForeignKey('paikkala.Zone', related_name='rows', on_delete=models.CASCADE)
+    zone = models.ForeignKey(
+        "paikkala.Zone", related_name="rows", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=64)
     start_number = models.IntegerField()
     end_number = models.IntegerField()
@@ -22,20 +24,20 @@ class Row(models.Model):
         blank=True,
         max_length=128,
         validators=[validate_number_set],
-        help_text='seat numbers to consider not part of the row; comma-separated integers',
+        help_text="seat numbers to consider not part of the row; comma-separated integers",
     )
 
     class Meta:
         unique_together = (
             (
-                'zone',
-                'name',
+                "zone",
+                "name",
             ),
         )
 
     def clean(self) -> None:
         if self.end_number < self.start_number:
-            raise ValidationError('end number must be greater than start number')
+            raise ValidationError("end number must be greater than start number")
         self.capacity = len(self.get_numbers())
 
     def save(self, **kwargs: Any) -> None:
@@ -44,11 +46,17 @@ class Row(models.Model):
         self.zone.cache_total_capacity(save=True)
 
     def __str__(self) -> str:
-        return f'{self.zone.room.name} – {self.zone.name} – {self.name}'
+        return f"{self.zone.room.name} – {self.zone.name} – {self.name}"
 
-    def get_numbers(self, additional_excluded_set: Optional[Set[int]] = None) -> List[int]:
+    def get_numbers(
+        self, additional_excluded_set: Optional[Set[int]] = None
+    ) -> List[int]:
         excluded_set = self.get_excluded_set().union(additional_excluded_set or ())
-        return [number for number in range(self.start_number, self.end_number + 1) if number not in excluded_set]
+        return [
+            number
+            for number in range(self.start_number, self.end_number + 1)
+            if number not in excluded_set
+        ]
 
     def get_excluded_set(self) -> Set[int]:
         return parse_number_set(self.excluded_numbers)
@@ -56,7 +64,7 @@ class Row(models.Model):
     def reserve(
         self,
         *,
-        program: 'Program',
+        program: "Program",
         count: int,
         user: Optional[AbstractBaseUser] = None,
         name: Optional[str] = None,
@@ -64,13 +72,15 @@ class Row(models.Model):
         phone: Optional[str] = None,
         attempt_sequential: bool = True,
         excluded_numbers: Optional[Set[int]] = None,
-    ) -> Iterator['Ticket']:
+    ) -> Iterator["Ticket"]:
         """
         Reserve N seats from this row.
 
         May return less than N seats if there are not enough seats available.
         """
-        reserved_numbers = set(program.tickets.filter(row=self).values_list('number', flat=True))
+        reserved_numbers = set(
+            program.tickets.filter(row=self).values_list("number", flat=True)
+        )
         unreserved_numbers = [
             number
             for number in self.get_numbers(additional_excluded_set=excluded_numbers)
@@ -81,18 +91,22 @@ class Row(models.Model):
             # Find runs of sequential numbers in the unreserved numbers.
             sequential_runs = find_runs(unreserved_numbers, following_integer)
             # Filter in only those that would have at least N seats left.
-            acceptable_sequential_runs = [run for run in sequential_runs if len(run) >= count]
+            acceptable_sequential_runs = [
+                run for run in sequential_runs if len(run) >= count
+            ]
             # If one was found, use it as the source of seat numbers to reserve.
             if acceptable_sequential_runs:
                 unreserved_numbers = acceptable_sequential_runs[0]
                 assert len(unreserved_numbers) >= count
 
+        tickets = []
         for _ in range(count):
             try:
                 number = unreserved_numbers.pop(0)
             except IndexError:
                 break
-            yield program.tickets.create(
+
+            tickets.append(Ticket(
                 row=self,
                 zone=self.zone,
                 user=user,
@@ -100,4 +114,11 @@ class Row(models.Model):
                 email=email,
                 phone=phone,
                 number=number,
-            )
+                program=program,
+                key=generate_key(),
+            ))
+
+        for t in tickets:
+            t.recompute_qualifiers()
+
+        yield from Ticket.objects.bulk_create(tickets)
